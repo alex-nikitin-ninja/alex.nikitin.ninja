69ecbc1002ec5532bc872d82fd1b20b4f2bf5a95
Alex <alex.nikitin.2015@gmail.com>
2016-12-29 04:55:19 -0800

    removed comments

--- actions/sendMail.php
+++ actions/sendMail.php
@@ -13,15 +13,9 @@
 		isset($_POST['subject']) &&
 		isset($_POST['message'])
 	){
-		// $_POST['name']
-		// $_POST['email']
-		// $_POST['subject']
-		// $_POST['message']
-
 		$r['result'] = sendMail('alex@nikitin.ninja', $_POST['email'], '[From Site] ' . $_POST['subject'], json_encode($_POST, JSON_PRETTY_PRINT));
 		$r['code'] = '200';
 	}
-
-	// sleep(2);
+	
 	echo json_encode($r);
 ?>
